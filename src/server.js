e0ded810e6ace3f9cfe7c84b6c1641ee4e8ae619
felipe antunes campos <felipe.a.campos7@aluno.senai.br>
2026-05-08 08:52:31 -0300

    feat: subindo arquivos limpos na V4

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import 'dotenv/config';
-import exemplosRoutes from './routes/exemploRoute.js';
-import PersonagemRoute from './routes/PersonagemRoute.js'
+import express from "express";
+import "dotenv/config";
+import exemplosRoutes from "./routes/exemploRoute.js";
+import PersonagemRoute from "./routes/PersonagemRoute.js";
+import simuladoRoutes from "./routes/simuladoRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('🚀 API funcionando');
+app.get("/", (req, res) => {
+  res.send("🚀 API funcionando");
 });
 
 // Rotas
-app.use('/api/exemplos', exemplosRoutes);
-app.use('/personagem', PersonagemRoute);
+app.use("/api/exemplos", exemplosRoutes);
+app.use("/personagem", PersonagemRoute);
+app.use("/simulados", simuladoRoutes);
 
 app.use((req, res) => {
-    res.status(404).json({ error: 'Rota não encontrada' });
+  res.status(404).json({ error: "Rota não encontrada" });
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
